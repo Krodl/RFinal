0b8aa802cded22b97b29719a74797015729070c8
Kordell <kct1@pct.edu>
2019-04-06 21:05:33 -0400

    Added cities data, trimmed it to usefulness

--- script.R
+++ script.R
@@ -1,1 +1,8 @@
-#kek
+#=========================================================================================================
+#                                           Final Project
+#                                              CIT460
+#                                 Kordell Teenie & James Temoshenko
+#=========================================================================================================
+#Cities from: https://simplemaps.com/data/us-cities
+cities <- data.frame(read.csv("./data/uscitiestrimmed.csv"))
+cities
