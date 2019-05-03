bc09c48e5e420031da92e384d3fdf0b6d8db8d43
James Temoshenko <jct6@pct.edu>
2019-05-03 13:55:43 -0400

    Updates.

--- app.R
+++ app.R
@@ -5,33 +5,49 @@ library(graphics)
 library(shiny)
 library(dplyr)
 
-### Import ###
+### IMPORT RAW DATA ###
 
 cities.data <- data.frame(read.csv("./data/uscitiestrimmed.csv"))
 income.data <- data.frame(read.csv("./data/kaggle_income.csv"))
 
-#still need data:
-healthcare.data
+healthcare.data <- data.frame(read.csv("./data/")) #still need data
 crime.data
 taxes.data
 housing.data
-recreational.data
+food.data #zomato data?
 entertainment.data
 civic.data
 college.data
+socialmedia.data
 
-### Prepare ###
+### PREPARE FOR SERVER ###
 
-income.min <- min(income.data$Mean)
-income.max <- max(income.data$Mean)
-income.data <- select(income.data,City,State,Zip,Mean)
-income.data %>% sapply(income.data$Mean, function(x){x-mean(x)})
+final.data <- select(cities.data,city,state_name,county_fips)
+
+#income
+income.data.min <- min(income.data$Mean)
+income.data.max <- max(income.data$Mean)
+income.data <- select(income.data,Zip_Code,Mean)
+
+#crime
+#taxes
+#housing
+#recreational
+#entertainment
+#food
+#civic
+#college
+#socialmedia
+
+## Final Preparations (this is the dataframe that is used in the server)
+final.data <- merge(final.data, income.data, by.x="county_fips", by.y="Zip_Code")
 
 ui <- fluidPage(titlePanel("censusVis"),
                 
                 sidebarLayout(
                   sidebarPanel(
-                    helpText("Importance of..."),
+                    
+                    helpText("Importance of... (Lower=less important, higher=more important)"),
                     
                     sliderInput(
                       "healthcare",
@@ -111,23 +127,28 @@ ui <- fluidPage(titlePanel("censusVis"),
                       min = 0,
                       max = 100,
                       value = 50
+                    ),
+                    sliderInput(
+                      "food",
+                      label = "Restauraunts:",
+                      min = 0,
+                      max = 100,
+                      value = 50
                     )
                   ),
                   
                   mainPanel(dataTableOutput('table'))
+                  
                 ))
-
 
 server <- function(input, output) {
   
   ### Calculate scores
   
-  
-  ### Format final score table
-  
+    final.data <- reactive({percent_rank(final.data$Mean) * input$income})
   
   ### Output
-  output$table <- renderDataTable(cities.scores)
+  output$table <- renderDataTable(final.data)
 }
 
 # Run the application
